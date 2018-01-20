c1aa66fecafff502d21596c359ded4222a2ab86c
hxsf <i@hxsf.me>
2018-01-20 18:34:52 +0800

    add init

---
+++ init.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+brew install stormssh
+brew install mycli pgcli
+brew install coreutils --with-gmp
+brew install findutils --with-default-names
+brew install gnu-indent --with-default-names
+brew install gnu-sed --with-default-names
+brew install gnutls
+brew install grep --with-default-names
+brew install gnu-tar --with-default-names
+brew install gawk
+brew install g++
+brew install gcc
+brew install fontforge
+brew install wget
+brew install jq
+brew install tmux
+brew install git --with-blk-sha1 --with-curl --with-gettext --with-openssl --with-pcre2 
+brew install yarn
+brew install caddy
+brew install go
+brew install ruby
+gem source -r https://rubygems.org/
+gem source -a http://mirrors.aliyun.com/rubygems/
+sudo gem install tmuxinator
+brew install zsh-completions
+brew install python3
+brew install oath-toolkit
+brew install qrencode
+brew install zbar
+brew install smartmontools
+brew install p7zip
+brew cask install qlcolorcode qlstephen qlmarkdown quicklook-json qlimagesize webpquicklook suspicious-package quicklookase qlvideo
+brew install watch
+brew install fish htop
+brew install httpie
+
+yarn global add typescript
+yarn global add browser-sync
+yarn global add ts-node
+yarn global add weekly-report
+yarn global add nodemon
+
